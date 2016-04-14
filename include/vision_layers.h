5a336ad8392936f0b346d41f2abfb614059ade98
Haboric-Hu <huanbang1993@gmail.com>
2016-04-13 23:08:45 -0400

    relu implemented

--- include/vision_layers.h
+++ include/vision_layers.h
@@ -9,23 +9,71 @@
 namespace Latte {
 
 class Convolution;
-//class Pooling;
+class ReLU;
+class Pooling; // implement the default max-pooling
+class Dropout;
+class Deconvolution;
+class Crop;
+class Split;
+class SoftmaxWithLoss;
+class Softmax;
+class Silence;
 
 class Convolution : public Layer {
   int num_output, pad = 0, kernel_size;
   Halide::Image<float> kernel;
-  public:
-    /* TODO need to also give it bias. So best give it all the blobs */
-    Convolution(std::string name, const caffe::ConvolutionParameter *param, 
-                const caffe::BlobProto *blob);
-    Halide::Image<float> convolve(Halide::Image<float> input);
+public:
+  /* TODO need to also give it bias. So best give it all the blobs */
+  Convolution(std::string name, const caffe::ConvolutionParameter *param, 
+              const caffe::BlobProto *blob);
+  Halide::Image<float> convolve(Halide::Image<float> input);
 };
 
-/*
-class Pooling : public Layers {
 
+class ReLU : public Layers {
+	int num_output, float negative_slope;
+public:
+	ReLU(std:string name, const caffe::ReLUParameter *param, 
+			 float negative_slope = 0.f);
+	Halide::Image<float> relu(Halide::Image<float> input);
 };
-*/
+
+
+
+// class Pooling : public Layers {
+
+// };
+
+
+
+// class Dropout : public Layers {
+
+// };
+
+
+// class Deconvolution : public Layers {
+
+// };
+
+// class Crop : public Layers {
+
+// };
+
+// class Split : public Layers {
+
+// };
+
+// class SoftmaxWithLoss : public Layers {
+
+// };
+
+// class Softmax : public Layers {
+
+// };
+
+// class Silence : public Layers {
+
+// };
 
 } /* namespace latte */
 
--- src/vision_layers.cc
+++ src/vision_layers.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 
 #include "Halide.h"
 #include "caffe.pb.h"
@@ -12,6 +13,7 @@ using namespace std;
 using namespace caffe;
 using namespace Halide;
 
+/* Definitions for class Convolution */
 Convolution::Convolution(string name, const ConvolutionParameter *param, 
                          const BlobProto *blob) {
   name = name;
@@ -39,8 +41,53 @@ Image<float> Convolution::convolve(Image<float> input) {
       kernel(r.x, r.y, r.z + z*channels) * clamped(x + r.x, y + r.y, r.z));
 
   /* TODO define schedule */
+  // Might have some problem with output width and height
   Image<float> output = convolution.realize(width, height, num_output);
   return output;
 }
+
+/* Definitions for class ReLU */
+ReLU::ReLU(string name, const ReLUParameter *param,
+                 float negative_slope) {
+  name = name;
+  num_output = param->num_output();
+  negative_slope = negative_slope;
+}
+
+/* Definitions for class Pooling */
+Image<float> ReLU::relu(Image<float> input) {
+  Func rectified("relu");
+  Var x("x"), y("y"), z("z");
+  int width = input.width();
+  int height = input.height();
+  int channels = input.channels();
+
+  rectified(x, y, z) = Halide::max(0, input(x, y, z)) + negative_slope *
+                       Halide::min(0, input(x, y, z));
+
+  Image<float> output = rectified.realize(width, height, num_output);
+  return output;
+}
+
+
+/* Definitions for class Dropout */
+
+
+/* Definitions for class Deconvolution */
+
+
+/* Definitions for class Crop */
+
+
+/* Definitions for class Split */
+
+
+/* Definitions for class SoftmaxWithLoss */
+
+
+/* Definitions for class Softmax */
+
+
+/* Definitions for class Silence */
 
 } /* namespace latte */
