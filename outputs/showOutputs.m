f18891e2ebad889715dd6ffd7840b868343b4a5b
Haboric-Hu <huanbang1993@gmail.com>
2016-04-15 23:30:35 -0400

    first result from conv, makes sense to me

---
+++ outputs/showOutputs.m
@@ -0,0 +1,22 @@
+%% Script for display montage of outputs
+fileFolder = fullfile(pwd);
+dirOutput = dir(fullfile(fileFolder,'channel*.txt'));
+fileNames = {dirOutput.name}';
+
+X = [];
+for i = 1:size(fileNames,1)
+    [A,delimiterOut] = importdata(fileNames{i});
+    X = cat(3, X, A);
+end
+
+% Compute width and height
+width = 8;
+height = ceil(size(X, 3) / width);
+
+figure
+for i = 1:size(X,3)
+    subplot(width,height,i);
+    axis off;
+    imagesc(X(:,:,i));
+    set(gca,'xtick',[],'ytick',[])
+end
