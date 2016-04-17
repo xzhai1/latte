6f288fca090d6d4fb4f651affb7dca689b57b3e0
Haboric-Hu <anbangh@andrew.cmu.edu>
2016-04-17 10:53:22 -0400

    loss debug 2

--- src/layers/loss_layers.cc
+++ src/layers/loss_layers.cc
@@ -24,6 +24,7 @@ Softmax::Softmax(string layer_name) {
 Image<float> 
 Softmax::run(Image<float> input) 
 {
+  #if 0
   Func prob;
   Func normalizer;
   Var x, y, z;
@@ -31,13 +32,43 @@ Softmax::run(Image<float> input)
   int height    = input.height();
   int channels  = input.channels();
 
+  cout << "create RDom" << endl;
   RDom r(0, 1, 0, 1, 0, channels);
-  prob(x, y, z) = exp(input(x, y, z));
-  normalizer(x, y, z) = sum(prob(x, y, z + r.z));
-  prob(x, y, z) = prob(x, y, z)/normalizer(x, y, z);
-
+  cout << "compute prob 0" << endl;
+  prob(x, y, z) = Halide::exp(input(x, y, z));
+  cout << "compute prob 1" << endl;
+  normalizer(x, y) = Halide::sum(prob(x, y, r.z));
+  cout << "compute prob 2" << endl;
+  prob(x, y, z) = prob(x, y, z)/normalizer(x, y);
+  normalizer.compute_at(prob, x);
   /* TODO: define schedule */
+  cout << "realize prob" << endl;
   Image<float> output = prob.realize(width, height, channels);
+  cout << "return" << endl;
+  return output;
+  #endif
+
+  Image<float> normalizer(input.width(), input.height(), 1);
+  Image<float> output(input.width(), input.height(), input.channels());
+  #pragma omp parallel for
+  for (int k = 0; k < input.channels(); k++) {
+    for (int j = 0; j < input.height(); j++) {
+      for (int i = 0; i < input.width(); i++) {
+        output(i, j, k) = exp(output(i, j, k));
+        normalizer(i, j) += output(i, j, k);
+      }
+    }
+  }
+
+  #pragma omp parallel for
+  for (int j = 0; j < input.height(); j++) {
+    for (int i = 0; i < input.width(); i++) {
+      for (int k = 0; k < input.channels(); k++) {
+        output(i, j, k) = output(i, j, k) / normalizer(i, j);
+      }
+    }
+  }
+
   return output;
 }
 
