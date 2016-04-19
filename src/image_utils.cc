771dadb33872783d672cb6d3902b761b593eee1b
Haboric-Hu <huanbang1993@gmail.com>
2016-04-18 21:36:58 -0400

    col2im implemented but not tested

--- src/image_utils.cc
+++ src/image_utils.cc
@@ -49,3 +49,44 @@ im2col(Image<float> input, int kernel_size, int pad, int stride)
 
   return output.realize(output_width, output_height);
 }
+
+
+
+Image<float>
+col2im(Image<float> output, int kernel_size, int pad, int stride, 
+  int width, int height, int channels)
+{
+  int input_width   = (width + 2 * pad - kernel_size) / stride + 1;
+  int input_height  = (height + 2 * pad - kernel_size) / stride + 1;
+  int input_channels = channels;
+
+  int x_steps = (input_width  + 2*pad - kernel_size)/stride + 1;
+  int y_steps = (input_height + 2*pad - kernel_size)/stride + 1;
+
+  int output_width = output.width();
+  int output_height = output.height();
+
+  /* Initialize output */
+  Image<float> input(input_width, input_height, input_channels);
+  for (int z = 0; z < input_channels; z++) {
+    for (int j = -pad; j < input_height+pad; j += stride) {
+      for (int i = -pad; i < input_width+pad; i += stride) {
+        for (int j_k = 0; j_k < kernel_size; j_k++) {
+
+          if (j+j_k >= 0 && j+j_k < input_height) {
+            for (int i_k = 0; i_k < kernel_size; i_k++) {
+
+              if (i+i_k >=0 && i+i_k < input_width) {
+                int out_x = (i+i_k) + (j+j_k)*input_width;
+                int out_y = i_k + j_k*kernel_size + z*kernel_size*kernel_size;
+                input(i+i_k, j+j_k, z) += output(out_x, out_y);
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  return input;
+}
--- tests/test_image_utils.cc
+++ tests/test_image_utils.cc
@@ -21,11 +21,20 @@ test_im2col(string image_path)
   int kernel_size = 3;
   int stride = 1;
   int pad = 1;
-  Image<float> output = im2col(input, kernel_size, pad, stride);
+  Image<float> output1 = im2col(input, kernel_size, pad, stride);
+
+  #if 0
   cout << "output dimension" << endl;
   cout << output.width() << endl;
   cout << output.height() << endl;
   save_image(output, "im2col.png");
+  #endif
+
+  Image<float> output2 = col2im(output1, kernel_size, pad, stride, input.width(), input.height(), input.channels());
+  cout << "output2 dimension" << endl;
+  cout << output2.width() << endl;
+  cout << output2.height() << endl;
+  save_image(output2, "col2im.png");
 
   return true;
 }
