8c1a78b1b64baf0bb7a8340311ab09068b078967
X.D. Zhai <xingda89@gmail.com>
2016-05-04 13:44:26 -0400

    python script can batch

--- python/test_conv.py
+++ python/test_conv.py
@@ -1,155 +1,64 @@
-
-# coding: utf-8
-
-# In[90]:
-
-# The caffe module needs to be on the Python path;
-#  we'll add it here explicitly.
 import sys
-caffe_root = '/home/anbangh/private/caffe/'  # this file should be run from {caffe_root}/examples (otherwise change this line)
-model_root = '/home/anbangh/private/fcn.berkeleyvision.org/'
-sys.path.insert(0, caffe_root + 'python')
+import argparse
 
-import caffe
-# If you get "No module named _caffe", either you have not built pycaffe or you have the wrong path.
 import numpy as np
-# get_ipython().magic(u'matplotlib inline')
-import matplotlib.pyplot as plt
 from PIL import Image
 import time
 
-# In[68]:
+def main(args):
+    caffe.set_mode_cpu()
 
-caffe.set_mode_cpu()
+    # Create the network
+    net = caffe.Net(args.deploy_prototxt, args.trained_caffemodel, caffe.TEST)
 
-#model_def = model_root + 'voc-fcn8s/deploy.prototxt'
-#model_weights = model_root + 'voc-fcn8s/fcn8s-heavy-pascal.caffemodel'
+    # create transformer for the input called 'data'
+    transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
+    transformer.set_transpose('data', (2,0,1))      # move image channels to outermost dimension
+    #transformer.set_mean('data', mu)                # subtract the dataset-mean value in each channel
+    transformer.set_raw_scale('data', 255)          # rescale from [0, 1] to [0, 255]
+    transformer.set_channel_swap('data', (2,1,0))   # swap channels from RGB to BGR
 
-model_def = model_root + 'voc-fcn32s/deploy.prototxt'
-model_weights = model_root + 'voc-fcn32s/fcn32s-heavy-pascal.caffemodel'
+    net.blobs['data'].reshape(args.batch,       # batch size
+                              3,                # 3-channel (BGR) images
+                              500, 500)         # image size is 227x227
+    image = caffe.io.load_image(args.image)
+    transformed_image = transformer.preprocess('data', image)
 
-net = caffe.Net(model_def,      # defines the structure of the model
-                model_weights,  # contains the trained weights
-                caffe.TEST)     # use test mode (e.g., don't perform dropout)
+    # copy the image data into the memory allocated for the net
+    for i in range(args.batch):
+        net.blobs['data'].data[i,:,:,:] = transformed_image
 
+    start = time.time()
+    net.forward(start='input', end='conv1_1')
 
-# In[ ]:
+    #net.forward()
 
+    end = time.time()
+    print end - start, "seconds"
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--caffe_python',
+                        help='path/to/caffe/python',
+                        required=True)
+    parser.add_argument('-d', '--deploy_prototxt',
+                        help='path to deploy.prototxt',
+                        required=True)
+    parser.add_argument('-t', '--trained_caffemodel',
+                        help='path to model.caffemodel',
+                        required=True)
+    parser.add_argument('-b', '--batch',
+                        type=int,
+                        help='batch size',
+                        required=True)
+    # TODO for now, just duplicate the image
+    parser.add_argument('-i', '--image',
+                        help='path to test image',
+                        required=True)
+    args = parser.parse_args()
 
+    # Make sure we can find the caffe python interface before importing it
+    sys.path.insert(0, args.caffe_python)
+    import caffe
 
-# In[125]:
-
-#im = Image.open('/home/xd/Projects/CMU/TrainVal/VOCdevkit/VOC2011/JPEGImages/2007_000129.jpg')
-im = Image.open('../images/cat.png')
-in_ = np.array(im, dtype=np.float32)
-print in_.shape
-in_ = in_[:,:,::-1]
-print in_.shape
-#in_ -= np.array((104.00698793,116.66876762,122.67891434))                          
-in_ = in_.transpose((2,0,1))
-print in_.shape
-
-
-# In[126]:
-
-print in_.shape
-input_data = net.blobs['data']
-
-for i in range(4):
-    print input_data.shape[i]
-
-
-# In[194]:
-
-weights = net.params['conv1_1'][0]
-#bias = net.params['conv1_1'][1]
-
-
-# In[195]:
-
-print weights.shape[0], weights.num
-print weights.shape[1], weights.channels
-print weights.shape[2], weights.height
-print weights.shape[3], weights.width
-weights.data.shape
-
-
-# In[168]:
-
-# shape for input (data blob is N x C x H x W), set data                           
-net.blobs['data'].reshape(1, *in_.shape)                                           
-net.blobs['data'].data[...] = in_
-
-print list(net._layer_names)
-# just run till the conv1_1 layer, inclusive
-#net.forward(start='input', end='conv1_1')
-
-start = time.time()
-net.forward()
-end = time.time()
-print end - start, "seconds"
-
-# In[169]:
-
-'''
-# result of the convolution, initialized to 0
-conv1_1_result = net.blobs['conv1_1']
-print conv1_1_result.data.shape
-
-
-# In[229]:
-
-type(conv1_1_result.data[0])
-
-
-# In[231]:
-
-# can save all layers here
-np.savetxt("caffe.csv", conv1_1_result.data[0, 0], delimiter=",")
-
-
-# In[213]:
-
-#filt_min, filt_max = conv1_1_result.data.min(), conv1_1_result.data.max()
-#plt.figure()
-#plt.imshow(conv1_1_result.data[0, 0], cmap='gray', vmin=filt_min, vmax=filt_max)
-#plt.imshow(conv1_1_result.data[0, 0], cmap='gray')
-#plt.axis('off')
-#plt.savefig("test.png")
-
-
-# In[217]:
-
-#caffe_data = conv1_1_result.data[0, 0]
-
-
-# In[218]:
-
-#halide_data = np.genfromtxt("../test.csv", delimiter=",")
-
-
-# In[219]:
-
-#caffe_data.shape
-
-
-# In[220]:
-
-#halide_data.shape
-
-
-# In[223]:
-
-#for x in range(734):
-#    for y in range(734):
-#        caffe_val = caffe_data[x, y]
-#        halide_val = caffe_data[x, y]
-#        if caffe_val != halide_val:
-#            print x, y
-
-
-# In[ ]:
-'''
-
-
+    main(args)
